4e067085770568b35fedca852f5eb654f264699d
dodocap <dodocap@naver.com>
2024-02-20 11:54:54 +0900

    feat: base process
    - packages
    - folders
    - app skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_provider/pages/todos_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo Provider',
+      title: 'TODOS',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const TodosPage(),
     );
   }
 }
