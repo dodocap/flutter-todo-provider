d60e0b7c1af35fe6624ae440d8444a60c39b57ad
dodocap <dodocap@naver.com>
2024-02-21 16:12:53 +0900

    feat: Injecting Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_provider/pages/todos_page.dart';
+import 'package:todo_provider/providers/providers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODOS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (_) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (_) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (_) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (_) => ActiveTodoCount(),
+          update: (_, todoList, activeTodoCount) {
+            return activeTodoCount!..update(todoList);
+          },
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList, FilteredTodos>(
+          create: (_) => FilteredTodos(),
+          update: (_, todoFilter, todoSearch, todoList, filteredTodos) {
+            return filteredTodos!..update(todoFilter, todoSearch, todoList);
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const TodosPage(),
       ),
-      home: const TodosPage(),
     );
   }
 }
